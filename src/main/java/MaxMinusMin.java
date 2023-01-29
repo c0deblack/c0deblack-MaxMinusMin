87e6b984831f914d9aed7c706be5f17cf33f30bf
c0deblack <79178809+c0deblack@users.noreply.github.com>
2023-01-29 00:56:33 +0000

    Completed tests.

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -2,6 +2,32 @@
 import java.util.List;
 
 public class MaxMinusMin {
+    private Integer min(List<Integer> nums)
+    {
+        if(nums == null)
+        {
+            throw new IllegalArgumentException("min(List): List cannot be empty");
+        }
+        Integer min = nums.get(0);
+        for(int i = 1; i < nums.size(); ++i)
+        {
+            if(nums.get(i) < min) min = nums.get(i);
+        }
+        return min;
+    }
+    private Integer max(List<Integer> nums)
+    {
+        if(nums == null)
+        {
+            throw new IllegalArgumentException("max(List): List cannot be empty");
+        }
+        Integer max = nums.get(0);
+        for(int i = 1; i < nums.size(); ++i)
+        {
+            if(nums.get(i) < max) max = nums.get(i);
+        }
+        return max;
+    }
     /**
      * Return the difference between the highest and lowest values of the List.
      * This will require you to know some of the List interface methods.
@@ -10,6 +36,16 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        Integer min, max;
+        min = max = nums.get(0);
+        for(int i = 1; i < nums.size(); ++i)
+        {
+            int current = nums.get(i);
+            if( current < min ) min = current;
+            if( current > max ) max = current;
+        }
+    
+        
+        return max - min;
     }
 }
